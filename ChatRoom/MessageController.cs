8c7d925c74c590145018af71fb8226e26b463e8f
hajarachatibi <achatibihajar@gmail.com>
2021-07-04 18:27:06 +0100

    fixed blinking

--- ChatRoom/MessageController.cs
+++ ChatRoom/MessageController.cs
@@ -45,5 +45,6 @@ namespace ChatRoom
             }
             return messages;
         }
+
     }
 }
--- ChatRoom/Room.cs
+++ ChatRoom/Room.cs
@@ -32,24 +32,38 @@ namespace ChatRoom
         //Fonction pour rafraichir les messages
         public void RefreshMessages()
         {
-            this.listView1.Items.Clear();
+            //pour eviter le blinking de l'ecran
+            int nb_msg = 0;
             foreach (Message message in MessageController.getMessages())
             {
                 if (message.message == "a quitté la discussion." || message.message == "a rejoint la discussion.")
-                {
-                    var msg = this.listView1.Items.Add(new ListViewItem(message.user.username + " " + message.message + "    " + message.created_at.ToString("dd/MM/yyyy HH:mm:ss")));
-                    msg.BackColor = Color.FromArgb(194, 196, 161);
-                }
+                    nb_msg++;
                 else
-                {
-                    this.listView1.Items.Add(new ListViewItem(message.user.username + ":" + message.message));
-                    var date = this.listView1.Items.Add(new ListViewItem(message.created_at.ToString("dd/MM/yyyy HH:mm:ss")));
-                    date.ForeColor = Color.Gray;
-                    date.Font = new Font(FontFamily.GenericSansSerif, 7.5F);
-                }
-
+                    nb_msg += 2;
             }
-            this.listView1.Items[listView1.Items.Count -1].EnsureVisible();
+
+            //Rafraichir les msgs s'il y en a un nouveau msg 
+            if (nb_msg != this.listView1.Items.Count)
+            {
+                this.listView1.Items.Clear();
+                foreach (Message message in MessageController.getMessages())
+                {
+                    if (message.message == "a quitté la discussion." || message.message == "a rejoint la discussion.")
+                    {
+                        var msg = this.listView1.Items.Add(new ListViewItem(message.user.username + " " + message.message + "    " + message.created_at.ToString("dd/MM/yyyy HH:mm:ss")));
+                        msg.BackColor = Color.FromArgb(194, 196, 161);
+                    }
+                    else
+                    {
+                        this.listView1.Items.Add(new ListViewItem(message.user.username + ":" + message.message));
+                        var date = this.listView1.Items.Add(new ListViewItem(message.created_at.ToString("dd/MM/yyyy HH:mm:ss")));
+                        date.ForeColor = Color.Gray;
+                        date.Font = new Font(FontFamily.GenericSansSerif, 7.5F);
+                    }
+
+                }
+                this.listView1.Items[listView1.Items.Count - 1].EnsureVisible();
+            }
         }
 
         //Fonction pour rafraichir les personnes connectees
@@ -68,6 +82,7 @@ namespace ChatRoom
         private void timer1_Tick(object sender, EventArgs e)
         {
             this.RefreshActiveList();
+            this.RefreshMessages();
         }
 
         private void button2_Click(object sender, EventArgs e)
